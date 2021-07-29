bbc02a047f59491d6428fdf26c3c96bf3bb66c47
Kazuhiro Sera <ksera@slack-corp.com>
2021-07-29 21:50:29 +0900

    Update example and tests

--- slack_discovery_sdk/client.py
+++ slack_discovery_sdk/client.py
@@ -49,6 +49,49 @@ class DiscoveryClient(BaseDiscoveryClient):
         removed at anytime.
     """
 
+    def auth_test(self, **kwargs) -> DiscoveryResponse:
+        """Checks authentication & identity."""
+        return self.api_call("auth.test", params=kwargs)
+
+    def oauth_v2_access(
+        self,
+        *,
+        client_id: str,
+        client_secret: str,
+        # This field is required when processing the OAuth redirect URL requests
+        # while it's absent for token rotation
+        code: Optional[str] = None,
+        redirect_uri: Optional[str] = None,
+        # This field is required for token rotation
+        grant_type: Optional[str] = None,
+        # This field is required for token rotation
+        refresh_token: Optional[str] = None,
+        **kwargs
+    ) -> DiscoveryResponse:
+        """Exchanges a temporary OAuth verifier code for an access token.
+        Args:
+            client_id (str): Issued when you created your application. e.g. '4b39e9-752c4'
+            client_secret (str): Issued when you created your application. e.g. '33fea0113f5b1'
+            code (str): The code param returned via the OAuth callback. e.g. 'ccdaa72ad'
+            redirect_uri (optional str): Must match the originally submitted URI
+                (if one was sent). e.g. 'https://example.com'
+            grant_type: The grant type. The possible value is only 'refresh_token' as of July 2021.
+            refresh_token: The refresh token for token rotation.
+        """
+        if redirect_uri is not None:
+            kwargs.update({"redirect_uri": redirect_uri})
+        if code is not None:
+            kwargs.update({"code": code})
+        if grant_type is not None:
+            kwargs.update({"grant_type": grant_type})
+        if refresh_token is not None:
+            kwargs.update({"refresh_token": refresh_token})
+        return self.api_call(
+            "oauth.v2.access",
+            params=kwargs,
+            auth={"client_id": client_id, "client_secret": client_secret},
+        )
+
     def discovery_conversations_info(
         self, *, token: Optional[str] = None, channel: str, team: str, **kwargs
     ) -> DiscoveryResponse:
--- slack_discovery_sdk/internal_utils.py
+++ slack_discovery_sdk/internal_utils.py
@@ -1,15 +1,7 @@
-import json
-import os
 import platform
 import sys
-import warnings
-from ssl import SSLContext
-from typing import Dict, Union, Optional, Any, Sequence
+from typing import Dict, Union, Optional, Any
 from urllib.parse import urljoin
-
-from slack_sdk.errors import SlackRequestError
-from slack_sdk.models.attachments import Attachment
-from slack_sdk.models.blocks import Block
 
 from . import version
 
@@ -60,130 +52,6 @@ def _get_url(base_url: str, api_method: str) -> str:
     return urljoin(base_url, api_method)
 
 
-def _get_headers(
-    *,
-    headers: dict,
-    token: Optional[str],
-    has_json: bool,
-    has_files: bool,
-    request_specific_headers: Optional[dict],
-) -> Dict[str, str]:
-    """Constructs the headers need for a request.
-    Args:
-        has_json (bool): Whether or not the request has json.
-        has_files (bool): Whether or not the request has files.
-        request_specific_headers (dict): Additional headers specified by the user for a specific request.
-    Returns:
-        The headers dictionary.
-            e.g. {
-                'Content-Type': 'application/json;charset=utf-8',
-                'Authorization': 'Bearer xoxb-1234-1243',
-                'User-Agent': 'Python/3.6.8 slack/2.1.0 Darwin/17.7.0'
-            }
-    """
-    final_headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    if headers is None or "User-Agent" not in headers:
-        final_headers["User-Agent"] = get_user_agent()
-
-    if token:
-        final_headers.update({"Authorization": "Bearer {}".format(token)})
-    if headers is None:
-        headers = {}
-
-    # Merge headers specified at client initialization.
-    final_headers.update(headers)
-
-    # Merge headers specified for a specific request. e.g. oauth.access
-    if request_specific_headers:
-        final_headers.update(request_specific_headers)
-
-    if has_json:
-        final_headers.update({"Content-Type": "application/json;charset=utf-8"})
-
-    if has_files:
-        # These are set automatically by the aiohttp library.
-        final_headers.pop("Content-Type", None)
-
-    return final_headers
-
-
-def _set_default_params(target: dict, default_params: dict) -> None:
-    for name, value in default_params.items():
-        if name not in target:
-            target[name] = value
-
-
-def _build_req_args(
-    *,
-    token: Optional[str],
-    http_verb: str,
-    default_params: dict,
-    params: dict,
-    headers: dict,
-    auth: dict,
-    ssl: Optional[SSLContext],
-    proxy: Optional[str],
-) -> dict:
-    if params is not None and isinstance(params, dict):
-        params = {k: v for k, v in params.items() if v is not None}
-        _set_default_params(params, default_params)
-    if json is not None and isinstance(json, dict):
-        _set_default_params(json, default_params)
-
-    token: Optional[str] = token
-    if params is not None and "token" in params:
-        token = params.pop("token")
-    if json is not None and "token" in json:
-        token = json.pop("token")
-    req_args = {
-        "headers": _get_headers(
-            headers=headers,
-            token=token,
-            request_specific_headers=headers,
-        ),
-        "params": params,
-        "ssl": ssl,
-        "proxy": proxy,
-        "auth": auth,
-    }
-    return req_args
-
-
-def _parse_web_class_objects(kwargs) -> None:
-    def to_dict(obj: Union[Dict, Block, Attachment]):
-        if isinstance(obj, Block):
-            return obj.to_dict()
-        if isinstance(obj, Attachment):
-            return obj.to_dict()
-        return obj
-
-    blocks = kwargs.get("blocks", None)
-    if blocks is not None and isinstance(blocks, list):
-        dict_blocks = [to_dict(b) for b in blocks]
-        kwargs.update({"blocks": dict_blocks})
-
-    attachments = kwargs.get("attachments", None)
-    if attachments is not None and isinstance(attachments, list):
-        dict_attachments = [to_dict(a) for a in attachments]
-        kwargs.update({"attachments": dict_attachments})
-
-
-def _update_call_participants(
-    kwargs, users: Union[str, Sequence[Dict[str, str]]]
-) -> None:
-    if users is None:
-        return
-
-    if isinstance(users, list):
-        kwargs.update({"users": json.dumps(users)})
-    elif isinstance(users, str):
-        kwargs.update({"users": users})
-    else:
-        raise SlackRequestError("users must be either str or Sequence[Dict[str, str]]")
-
-
 def _next_cursor_is_present(data) -> bool:
     """Determine if the response contains 'next_cursor'
     and 'next_cursor' is not empty.
@@ -203,43 +71,6 @@ def _to_0_or_1_if_bool(v: Any) -> Union[Any, str]:
     if isinstance(v, bool):
         return "1" if v else "0"
     return v
-
-
-def _warn_if_text_is_missing(endpoint: str, kwargs: Dict[str, Any]) -> None:
-    missing = "text"
-    attachments = kwargs.get("attachments")
-    # Note that this method does not verify attachments
-    # if the value is already serialized as a single str value.
-    if attachments is not None and isinstance(attachments, list):
-        # https://api.slack.com/reference/messaging/attachments
-        # Check if the fallback field exists for all the attachments
-        if all(
-            [
-                isinstance(attachment, dict)
-                and len(attachment.get("fallback", "").strip()) > 0
-                for attachment in attachments
-            ]
-        ):
-            # The attachments are all good
-            return
-        missing = "fallback"
-    else:
-        text = kwargs.get("text")
-        if text and len(text.strip()) > 0:
-            # Note that this is applicable only for blocks.
-            return
-
-    message = (
-        f"The `{missing}` argument is missing in the request payload for a {endpoint} call - "
-        f"It's a best practice to always provide a `{missing}` argument when posting a message. "
-        f"The `{missing}` argument is used in places where content cannot be rendered such as: "
-        "system push notifications, assistive technology such as screen readers, etc."
-    )
-    # for unit tests etc.
-    skip_deprecation = os.environ.get("SKIP_SLACK_SDK_WARNING")
-    if skip_deprecation:
-        return
-    warnings.warn(message, UserWarning)
 
 
 def _build_unexpected_body_error_message(body: str) -> str:
