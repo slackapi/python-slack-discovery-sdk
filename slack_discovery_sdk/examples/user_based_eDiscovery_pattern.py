f39ffb8af138dc5d7325e013bd477c0e1a9336f6
Horea Porutiu <hporutiu@slack-corp.com>
2021-09-29 15:25:16 -0700

    adding working user based eDiscovery pattern

---
+++ slack_discovery_sdk/examples/user_based_eDiscovery_pattern.py
@@ -0,0 +1,71 @@
+# Copyright 2021, Slack Technologies, LLC. All rights reserved.
+
+import logging, os, re, json
+from slack_discovery_sdk import DiscoveryClient
+from utils import export_json_to_file
+
+logging.basicConfig(level=logging.DEBUG)
+
+enterprise_token = os.environ["SLACK_DISCOVERY_SDK_TEST_ENTERPRISE_TOKEN"]
+
+client = DiscoveryClient(token=enterprise_token)
+
+DLP_LOGS_FILENAME = 'dlp_logs_output_'
+EDISCOVERY_LOGS_FILENAME = 'ediscovery_logs_output_'
+AUDIT_LOGS_FILENAME = 'audit_logs_output_'
+CONVERSATIONS_RECENT_FILENAME = 'discovery_conversations_recent'
+CONVERSATIONS_HISTORY_FILENAME = 'discovery_conversations_history'
+EDITS_HISTORY_FILENAME = 'discovery_conversations_edits'
+
+### CALL PATTERN OVERVIEW - User Based Discovery 
+## Step 1 - We need a userID. This will be the user which we will query to see their conversations.
+## Step 2 - Retrieve a list of conversation IDs a user is a member of using discovery.user.conversations
+## Step 3 - Retrieve the message history for each conversation returned in Step 2 using discovery.conversations.history
+
+
+## Step 1 - Get the user ID of the user you'd like to see conversation history from
+
+auth_test = client.auth_test()
+user_id = auth_test["user_id"]
+
+## Step 2 - Use `discovery.user.conversations` to retrieve a list of conversation ID's 
+# a user is a member of. We use limit of 500 here. 
+
+list_of_conversations = client.discovery_user_conversations(
+    user=user_id, limit=500
+)
+
+## Step 3 - Retrieve the message history for each conversation returned in Step 2 using discovery.conversations.history
+
+print('listing convos')
+print(list_of_conversations)
+
+output_info = []
+
+for conversation in list_of_conversations["channels"]:
+    # print(conversation)
+    #{'id': 'D02EZ1KTRF0', 'team_id': 'T02752RBD2R', 'date_joined': 1632242279, 'date_left': 0,
+    # 'is_private': True, 'is_im': True, 'is_mpim': False, 'is_ext_shared': False}
+    
+    channel_id = conversation["id"]
+    team_id = conversation["team_id"]
+    output_info.append((channel_id, team_id))
+
+    channel_conversation = client.discovery_conversations_history(
+        channel=channel_id, team=team_id
+    )
+    #print(channel_conversation)
+    print('$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    print(channel_conversation)
+    channel_conversation_json = json.dumps(channel_conversation.body, indent=4)
+    export_json_to_file(channel_conversation_json, CONVERSATIONS_HISTORY_FILENAME, channel_id, user_id)
+
+    ##{'ok': True, 'messages': 
+    # [{'client_msg_id': '6d917772-0b8a-4621-902c-ef85d9fd9987', 'type': 'message', 'text': 'affff', 'user': 'U0271AV54NS',
+    # 'ts': '1627515227.000200', 'team': 'T02752RBD2R', 'blocks': 
+    # [{'type': 'rich_text', 'block_id': 'YIj', 'elements': [{'type': 'rich_text_section', 'elements': 
+    # [{'type': 'text', 'text': 'affff'}]}]}]}, {'client_msg_id': 'fbdaaba7-0abb-4323-8e02-7e7bc92c50ca', 
+    # 'type': 'message', 'text': 'hello', 'user': 'U0271AV54NS', 'ts': '1625604656.000200', 'team': 'T02752RBD2R', 
+    # 'blocks': [{'type': 'rich_text', 'block_id': 'WbOSU', 'elements': [{'type': 'rich_text_section', 'elements': [{'type': 'text', 'text': 'hello'}]}]}]}], 'has_edits': False}
+
+    
--- slack_discovery_sdk/examples/utils.py
+++ slack_discovery_sdk/examples/utils.py
@@ -1,30 +1,60 @@
 # Copyright 2021, Slack Technologies, LLC. All rights reserved.
 
-import platform, string, random, time, datetime, logging, os, re, pathlib, json, sys, time
+import platform, string, random, time, datetime, logging, os, re, pathlib, json, sys, time, shutil
 
 from typing import Dict, Union, Optional, Any
 from urllib.parse import urljoin
 
 FILE_EXTENSION = '.json'
 
-def export_json_to_file(new_items, logs_type):
+## This file contains functions which will help create a directory structure to organize and 
+## hold the output from the discovery APIs. This is just meant as a helper to get you started, 
+## and is by no means a production ready solution. Use this to get familiar with the discovery APIs.
+
+def export_json_to_file(new_items, logs_type, channel_id, user_id):
     """This method is used to save data from the eDiscovery, DLP, and SIEM call patterns.
     This is just for example use, to make our partners and customers life easier. This is 
     not meant to be a production implementation by any means.
     Args:
         new_items: this is the JSON response from the discovery APIs that we want to save.
+        logs_type: this is the discovery API method used
+        channel_id: channel_id
     Returns:
         None
     """
 
-    todays_folder = create_folder()
+    if channel_id != None:
+        channel_folder = create_folder_for_channel(channel_id, user_id, logs_type)
 
     file_name = logs_type + FILE_EXTENSION
-
-    with open(os.path.join(todays_folder, file_name), "a") as outfile:
+    new_items = json.dumps(new_items, indent=4)
+    with open(os.path.join(channel_folder, file_name), "a") as outfile:
         outfile.write(new_items)
 
     return None
+
+def create_folder_for_channel(channel_id, user_id, logs_type) -> str:
+    """This method will create a folder to keep all output from the discovery calls 
+    organized. It will create a folder based on channelID for a particular user: for example C02642201. 
+    Args:
+        None
+    Returns:
+        Str which resresents the channel
+    """
+
+    current_day = datetime.date.today()
+
+    formatted_date = datetime.date.strftime(current_day, "%m_%d_%Y")
+
+    path = formatted_date + "/" + user_id + "/" + channel_id + "/" + logs_type
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        shutil.rmtree(path)           # Removes all the subdirectories!
+        os.makedirs(path)
+
+    return formatted_date + "/" + user_id + "/" + channel_id + "/" + logs_type
 
 def is_credit_card_number(credit_card) -> bool:
     valid_structure = r"[456]\d{3}(-?\d{4}){3}$"
@@ -35,76 +65,3 @@ def is_credit_card_number(credit_card) -> bool:
         return True
     else:
         return False
-
-def get_current_timestamp() -> float:
-    ts = time.time()
-    return ts
-
-def get_random_string(length: int = 10) -> str:
-    all_ascii_lowercase_chars = string.ascii_lowercase
-    random_string = "".join(random.choice(all_ascii_lowercase_chars) for _ in range(length))
-    return random_string
-
-def get_date_today() -> str:
-    current_day = datetime.date.today()
-    print("\n Default Date Object:", current_day, "\n")
- 
-    formatted_date = datetime.date.strftime(current_day, "%m_%d_%Y")
-    print("\n Formatted Date String:", formatted_date, "\n")
-    return formatted_date
-
-def create_folder() -> str:
-    """This method will create a folder to keep all output from the discovery calls 
-    organized. It will create a folder based on MM-DD-YYYY United States date format. 
-    Args:
-        None
-    Returns:
-        None
-    """
-
-    current_day = datetime.date.today()
-
-    formatted_date = datetime.date.strftime(current_day, "%m_%d_%Y")
-    print("\n Formatted Date String:", formatted_date, "\n")
-
-    todays_date_path = formatted_date + '_eDiscovery_output'
-    print(todays_date_path)
-    if not os.path.exists(todays_date_path):
-        os.makedirs(todays_date_path)
-
-    return todays_date_path
-
-
-def get_timestamp_last_minute() -> int:
-    now = time.time()
-    seconds_in_one_minute = 60
-    one_minute_ago = now - seconds_in_one_minute
-    return int(one_minute_ago)
-
-def create_draft() -> any:
-    kwargs = None
-
-    blocks = [{
-		"block_id": "",
-		"elements": [{
-			"elements": [{
-				"text": "testing the create draft API",
-				"type": "text"
-			}],
-			"type": "rich_text_section"
-		}],
-		"type": "rich_text"
-	}]
-
-    kwargs.update(
-            {
-                "blocks": blocks,
-                "client_msg_id": client_msg_id,
-                "destinations": destinations,
-                "file_ids": file_ids,
-                "is_from_composer": is_from_composer,
-            }
-        )
-    return self.api_call(
-            "drafts.create", http_method="POST", params=kwargs
-        )
