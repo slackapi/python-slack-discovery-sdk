6d929cd9ec10a33eb9f21f16824aa56fb37948d7
Horea Porutiu <hporutiu@slack-corp.com>
2021-10-11 10:09:55 -0700

    adding examples with DLP, audit logs, eDiscovery

---
+++ slack_discovery_sdk/examples/DLP_call_pattern.py
@@ -0,0 +1,61 @@
+# Copyright 2021, Slack Technologies, LLC. All rights reserved.
+
+import logging, os
+from slack_discovery_sdk import DiscoveryClient
+from slack_sdk import WebClient
+
+from utils import is_credit_card_number
+
+from tests.env_variable_names import (
+    SLACK_DISCOVERY_SDK_TEST_CHANNEL_ID,
+    SLACK_DISCOVERY_SDK_TEST_ENTERPRISE_TOKEN,
+    SLACK_DISCOVERY_SDK_TEST_BOT_TOKEN
+)
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Initialize the client
+enterprise_token = os.environ[SLACK_DISCOVERY_SDK_TEST_ENTERPRISE_TOKEN]
+test_channel = os.environ[SLACK_DISCOVERY_SDK_TEST_CHANNEL_ID]
+client = DiscoveryClient(token=enterprise_token)
+# store the auth_test response, use it for the User_id field later
+auth_test = client.auth_test()
+
+#initialize the web client, used for chat.postMessage
+web_client = WebClient(
+    token=os.environ[SLACK_DISCOVERY_SDK_TEST_BOT_TOKEN]
+)
+
+tombstone_msg = web_client.chat_postMessage(
+    channel=test_channel,
+    text="5122-2368-7954-3214",
+)
+
+# CALL PATTERN - Data Loss Prevention
+# Step 1 - Retrieve a list of the most recent conversations in the last 24 hours.
+# change parameters to run the function every 10 seconds (or desired interval)
+# using the latest params
+
+last_24_hour_conversations = client.discovery_conversations_recent(
+    user=auth_test["user_id"], limit=500
+)
+
+# Step 2 - Call conversations_history on each of the previous conversations, to grab the text from each
+# conversation from each channel
+
+for conversation in last_24_hour_conversations["channels"]:
+    channel_conversation = client.discovery_conversations_history(
+        channel=conversation["id"], team=conversation["team"]
+    )
+
+    for message in channel_conversation["messages"]:
+
+        # Step 3 - check each message and see if it contains a credit card number. If it does, tombstone the message.
+        # A valid credit card for our logic is something like the following: '5122-2368-7954-3214'
+        # Please note this is just example logic, to help you understand how to use the Discovery APIs.
+
+        if is_credit_card_number(message["text"]):
+            client.discovery_chat_tombstone(
+                ts=message["ts"],
+                channel=conversation["id"],
+            )
---
+++ slack_discovery_sdk/examples/utils.py
@@ -0,0 +1,68 @@
+# Copyright 2021, Slack Technologies, LLC. All rights reserved.
+
+import datetime, os, re, shutil
+
+FILE_EXTENSION = ".json"
+
+## This file contains functions which will help create a directory structure to organize and
+## hold the output from the discovery APIs. This is just meant as a helper to get you started,
+## and is by no means a production ready solution. Use this to get familiar with the discovery APIs.
+
+
+def export_json_to_file(new_items, logs_type, channel_id, user_id):
+    """This method is used to save data from the eDiscovery, DLP, and SIEM call patterns.
+    This is just for example use, to make our partners and customers life easier. This is
+    not meant to be a production implementation by any means.
+    Args:
+        new_items: this is the JSON response from the discovery APIs that we want to save.
+        logs_type: this is the discovery API method used
+        channel_id: channel_id i.e C02449123
+        user_id: The ID of the user i.e. U09123411
+    Returns:
+        None
+    """
+
+    if channel_id != None:
+        channel_folder = create_folder_for_channel(channel_id, user_id)
+
+    file_name = logs_type + FILE_EXTENSION
+
+    with open(os.path.join(channel_folder, file_name), "a") as outfile:
+        outfile.write(new_items)
+
+    return None
+
+
+def create_folder_for_channel(channel_id, user_id) -> str:
+    """This method will create a folder to keep all output from the discovery calls
+    organized. It will create a folder based on channelID for a particular user: for example C02642201.
+    Args:
+        None
+    Returns:
+        Str which resresents the channel
+    """
+
+    current_day = datetime.date.today()
+
+    formatted_date = datetime.date.strftime(current_day, "%Y/%m/%d")
+
+    path = formatted_date + "/" + user_id + "/" + channel_id + "/"
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        shutil.rmtree(path)  # Removes all the subdirectories!
+        os.makedirs(path)
+
+    return formatted_date + "/" + user_id + "/" + channel_id + "/"
+
+
+def is_credit_card_number(credit_card) -> bool:
+    valid_structure = r"[456]\d{3}(-?\d{4}){3}$"
+    no_four_repeats = r"((\d)-?(?!(-?\2){3})){16}"
+    filters = valid_structure, no_four_repeats
+
+    if all(re.match(f, credit_card) for f in filters):
+        return True
+    else:
+        return False
