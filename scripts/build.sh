50aef747746de2dd55b3689b1ec3b440d10d6cf3
Kazuhiro Sera <ksera@slack-corp.com>
2021-10-15 07:22:43 +0900

    Add a script to build a package (#6)

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+script_dir=`dirname $0`
+cd ${script_dir}/..
+version=`grep __version__ ./slack_discovery_sdk/version.py | awk -F'"' '{print $2}'`
+while true; do
+    read -p "Do you wish to build a package for version ${version}? (y/N)" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+pip install twine wheel && \
+  rm -rf ./dist/ ./build/ *.egg-info/ && \
+  python setup.py sdist bdist_wheel && \
+  twine check dist/*
