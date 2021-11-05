29f3ec53a78628f3c6ec050071ec7bb02b740c92
Horea Porutiu <hporutiu@slack-corp.com>
2021-11-05 09:46:00 -0700

    edit script to take out slack_bolt and replace with slack_discovery_sdk

--- scripts/deploy_to_prod_pypi_org.sh
+++ scripts/deploy_to_prod_pypi_org.sh
@@ -4,11 +4,11 @@
 
 script_dir=`dirname $0`
 cd ${script_dir}/..
-rm -rf ./slack_bolt.egg-info
+rm -rf ./slack_discovery_sdk.egg-info
 
 pip install -U pip && \
   pip install twine wheel && \
-  rm -rf dist/ build/ slack_bolt.egg-info/ && \
+  rm -rf dist/ build/ slack_discovery_sdk.egg-info/ && \
   python setup.py sdist bdist_wheel && \
   twine check dist/* && \
   twine upload dist/*
