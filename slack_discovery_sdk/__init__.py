1c41241055cf58440702cb44d28868d336d646ef
Kazuhiro Sera <seratch@gmail.com>
2022-05-14 09:52:17 +0900

    Add __all__ to __init__.py

--- slack_discovery_sdk/__init__.py
+++ slack_discovery_sdk/__init__.py
@@ -3,5 +3,10 @@
 
 """The Slack Web API allows you to build applications that interact with Slack
 in more complex ways than the integrations we provide out of the box."""
-from .client import DiscoveryClient  # noqa
-from .oauth import DiscoveryOAuthApp  # noqa
+from .client import DiscoveryClient
+from .oauth import DiscoveryOAuthApp
+
+__all__ = [
+    "DiscoveryClient",
+    "DiscoveryOAuthApp",
+]
